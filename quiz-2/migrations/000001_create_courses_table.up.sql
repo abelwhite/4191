d4ee7d35f4b371fa2045d709ea60499eff3da448
Abel Blanco <2018117977@ub.edu.bz>
2023-03-26 16:48:59 -0600

    fixed migrations insert data

--- quiz-2/migrations/000001_create_courses_table.up.sql
+++ quiz-2/migrations/000001_create_courses_table.up.sql
@@ -2,6 +2,5 @@ CREATE TABLE courses(
     course_code VARCHAR(100) NOT NULL,
 	course_title VARCHAR(100) NOT NULL,
 	course_credit VARCHAR(200) NOT NULL
-	-- Version serial,
-	-- created_at TIMESTAMP(0) WITH TIME ZONE NOT NULL DEFAULT NOW()
+	
 );
--- quiz-2/migrations/000002_insert_courses_data.up.sql
+++ quiz-2/migrations/000002_insert_courses_data.up.sql
@@ -1,2 +1,2 @@
-COPY courses FROM 'courses.csv' WITH delimiter ',' CSV HEADER;
+COPY courses FROM '/home/abel/4191/quiz-2/courses.csv' WITH delimiter ',' CSV HEADER;
 
